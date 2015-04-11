f4f8e10b6f0955ad98ed0aa6f578d6116c2f72cd
Cyrille Perois <cperois@outlook.com>
2015-04-11 15:10:35 +0200

    Refonte page de login

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 
-gulp.task('watch', function() {
+gulp.task('watch', ['compile'], function() {
     gulp.watch('assets/scss/**/*.scss', ['styles']);
+    gulp.watch('assets/img/**/*', ['images']);
 });

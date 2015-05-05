d1cde7519b953c7c2052c07bdecea9a67155737b
Laura Vandeville <laura.vandeville@hotmail.fr>
2015-05-05 12:21:49 +0200

    Sauvegarde script bugguée

--- assets/js/modules/EditorApp.jsx
+++ assets/js/modules/EditorApp.jsx
@@ -31,7 +31,42 @@ var EditorApp = React.createClass({
 	},
 
 	handleSaveClick: function() {
-		console.log('Saving the script...');
+		var xhr = new XMLHttpRequest();
+		var params = 'scriptContent=' + this.state.script;
+		var scriptName;
+		var response;
+
+		console.log(React.findDOMNode(this.refs.scriptId).value);
+
+		if (React.findDOMNode(this.refs.scriptId).value !== '') {
+			params += '&scriptId=' + React.findDOMNode(this.refs.scriptId).value;
+		} else {
+			while (!scriptName) {
+				scriptName = prompt('Nom du script : ');
+			}
+		}
+
+		xhr.open('POST', '/script/save', true);
+		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState === 4) {
+				if (xhr.status === 200) {
+					response = JSON.parse(xhr.responseText);
+					if (response.meta.code === 201) {
+						history.pushState({}, 'editor', '/editor/' + response.data.scriptId);
+						this.setState({
+							scriptId: response.data.scriptId
+						});
+					} else if (response.meta.code === 200) {
+						console.log('saved');
+					}
+				} else {
+					console.log('Error : ' + xhr.responseText);
+				}
+			}
+		}.bind(this);
+
+		xhr.send(params);
 	},
 
 	handleScriptChange: function(event, content) {
@@ -44,13 +79,15 @@ var EditorApp = React.createClass({
 		return {
 			script: '',
 			result: '',
-			graphs: []
+			graphs: [],
+			scriptId: ''
 		};
 	},
 
 	render: function() {
 		return (
 			<div id="editor">
+				<input type="hidden" value={this.state.scriptId} ref="scriptId" />
 				<EditorMenu handleRunClick={this.handleRunClick} handleSaveClick={this.handleSaveClick} />
 				<Editor script={this.state.script} onChange={this.handleScriptChange} />
 				<EditorResult result={this.state.result} graphs={this.state.graphs} />
--- assets/js/modules/EditorMenu.jsx
+++ assets/js/modules/EditorMenu.jsx
@@ -6,8 +6,13 @@ var EditorMenu = React.createClass({
 	render: function() {
 		return (
 			<div className="editor-menu">
-				<button className="editor-menu-item" onClick={this.props.handleRunClick} type="button">Run</button>
-				<button className="editor-menu-item" onClick={this.props.handleSaveClick} type="button">Save</button>
+				<div className="editor-menu-left">
+					<button className="editor-menu-item" onClick={this.props.handleRunClick} type="button">Run</button>
+					<button className="editor-menu-item" onClick={this.props.handleSaveClick} type="button">Save</button>
+				</div>
+				<div className="editor-menu-right">
+					<a href="/" className="editor-menu-item">Dashboard</a>
+				</div>
 			</div>
 		);
 	}

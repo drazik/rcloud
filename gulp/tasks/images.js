ed2b9c2f5cf6cae79dfb00e986cba0a70a54b483
unknown <Cyrille Perois>
2015-05-05 21:05:24 +0200

    Suppression de gulp-imagemin et ses plugins

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,10 +1,6 @@
 var gulp = require('gulp');
-var plumber = require('gulp-plumber');
-var imagemin = require('gulp-imagemin');
 
 gulp.task('images', function() {
     gulp.src('assets/img/**/*')
-        .pipe(plumber())
-        .pipe(imagemin())
         .pipe(gulp.dest('web/assets/img'));
 });

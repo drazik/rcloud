227903c45240e77df872b24d08fdf1c622822c20
Cyrille Perois <cperois@outlook.com>
2015-06-17 23:35:57 +0200

    Réécriture de listAction pour n'afficher que les groupes dont l'utilisateur est owner

--- src/RCloud/Bundle/UserBundle/Controller/GroupController.php
+++ src/RCloud/Bundle/UserBundle/Controller/GroupController.php
@@ -7,4 +7,16 @@ use FOS\UserBundle\Controller\GroupController as BaseController;
 
 class GroupController extends BaseController
 {
+    public function listAction()
+    {
+        $user = $this->get('security.context')->getToken()->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('RCloudUserBundle:Group');
+
+        $groups = $repository->findBy(array('owner' => $user));
+
+        return $this->render('FOSUserBundle:Group:list.html.twig', array(
+            'groups' => $groups
+        ));
+    }
 }

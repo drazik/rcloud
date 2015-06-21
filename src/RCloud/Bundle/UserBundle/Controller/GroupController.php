0ea5ef03480c4a2097ac2548f32acba4ad492e00
Cyrille Perois <cperois@outlook.com>
2015-06-21 11:39:06 +0200

    Création de la méthode invite #21

--- src/RCloud/Bundle/UserBundle/Controller/GroupController.php
+++ src/RCloud/Bundle/UserBundle/Controller/GroupController.php
@@ -5,6 +5,10 @@ namespace RCloud\Bundle\UserBundle\Controller;
 
 use FOS\UserBundle\Controller\GroupController as BaseController;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
 class GroupController extends BaseController
 {
     public function listAction()
@@ -14,11 +18,20 @@ class GroupController extends BaseController
         $repository = $em->getRepository('RCloudUserBundle:Group');
 
         $ownedGroups = $repository->findBy(array('owner' => $user));
-        $belongToGroups = [];
+        $belongToGroups = $user->getGroups();
 
         return $this->render('FOSUserBundle:Group:list.html.twig', array(
             'ownedGroups' => $ownedGroups,
             'belongToGroups' => $belongToGroups
         ));
+    }
+
+    /**
+     * @Route("/invite", name="r_cloud_user_group_invite")
+     * @Template()
+     */
+    public function inviteAction()
+    {
+        return array();
     }
 }
